04bd508555c4bcfde71b9e250fc6c7d70da32bfb
East <eastmaels@gmail.com>
2019-01-21 23:49:35 +0900

    expire accounts after 24 hours

--- models/account.js
+++ models/account.js
@@ -2,7 +2,7 @@ let mongoose = require('mongoose')
 
 let accountSchema = new mongoose.Schema({
   account: String,
-  expire_at: {type: Date, default: Date.now, expires: 10} 
+  expire_at: { type: Date, default: Date.now, expires: 86400 } 
 })
 
 module.exports = mongoose.model('Account', accountSchema)
