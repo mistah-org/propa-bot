463da1879a6adf658248bb9f93e53c25f88d4d4d
East Mael <eastmaels@gmail.com>
2019-02-23 16:47:15 +0900

    updated sp memo

--- memo/index.js
+++ memo/index.js
@@ -1,13 +1,6 @@
 exports.templates = {}
 exports.templates['sp'] = `
-Quiero ayudar a todos los hispanohablantes a votar su comentario. Tengo más de 23,000 Steem Power, no era mucho, pero tal vez mis votos tengan algún significado para alguien.
-
-Lo que debe hacer es...
-1. Mire el video tema del día publicado en www.steemit.com/@fatimajunio
-
-2. Haga un comentario como un reflejo de lo que comprende
-
-3. Si puede hacer un comentario de video sobre el tema es mejor para recibir votos más grandes. Puedes usar @dtube o youtube o @dsound.
+Les doy dinero gratis a todos los que vean y comenten mi video todos los días.
 
 Te invito a unirte también a nuestro chat Discord.
 https://discord.gg/vzHFNd6
